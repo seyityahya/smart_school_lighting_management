11dcd7d6ea319b93daf8862e2e58a70c19798f2a
seyityahya <seyityahya05@gmail.com>
2023-04-16 15:11:55 +0300

    ledDurum get request işleminin yapılması

--- esp8266.ino
+++ esp8266.ino
@@ -1,48 +1,55 @@
-#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
 
+// WiFi bağlantısı
 const char* ssid = "seyit1";
-const char* password2 = "seyityahya4324";
-const char* serverAddress = "http://localhost:5000/auth/register";
+const char* password = "seyityahya4324";
 
+// Aracı sunucu adresi
+const char* serverName = "http://localhost:5000/api/ledAc";
+
+// LED pin numarası
+const int ledPin = 2;
 
 void setup() {
+  // Seri port başlatma
   Serial.begin(115200);
 
-  WiFi.begin(ssid, password2);
-  
-
+  // WiFi bağlantısı kurma
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.println("WiFi bağlanıyor...");
   }
+  Serial.println("WiFi bağlantısı başarılı.");
 
-  Serial.println("Connected to WiFi");
+  // LED pinini çıkış yapma
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // Örnek sensör verisi
-  String email = "deneme@deneme.com";
-  String password = "12345689";
-  String name = "enes gücük";
-  String role = "user";
-
-
-  // HTTP POST isteği oluştur
-  WiFiClient wifiClient; 
+  // HTTP isteği oluşturma
   HTTPClient http;
-  http.begin(wifiClient, serverAddress);
-  http.addHeader("Content-Type", "application/json");
+  http.begin(serverName);
 
+  // HTTP isteği gönderme ve yanıtı alıp yazdırma
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println(payload);
 
- String requestBody = String("{\"name\":") + String("\"") + name + String("\"") + String(",\"email\":") + String("\"") + email + String("\"") + String(",\"password\":") + String("\"") + password + String("\"") + String(",\"role\":") +String("\"") + role + String("\"") + String("}");
-  int httpResponseCode = http.POST(requestBody);
+    // "led_durumu" adlı değişkenin değerine göre LED'i aç veya kapat
+    if (payload == "true") {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+  } else {
+    Serial.println("HTTP isteği başarısız.");
+  }
 
-
-  Serial.print("HTTP response code: ");
-  Serial.println(httpResponseCode);
-
+  // HTTP isteğini sonlandırma ve 5 saniye bekleyerek tekrarlama
   http.end();
-
   delay(5000);
 }
