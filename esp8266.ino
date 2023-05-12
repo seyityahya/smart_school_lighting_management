ebea83f6e96e18f4442fb2756e1b80e13c9d2bf8
seyityahya <seyityahya05@gmail.com>
2023-05-12 14:54:02 +0300

    esp8266 kod kısmı düzeltildi

--- esp8266.ino
+++ esp8266.ino
@@ -7,12 +7,14 @@ const char* ssid = "seyit1";
 const char* password = "seyityahya4324";
 
 // Aracı sunucu adresi
-const char* server8A = "http://192.168.1.102:5000/api/siniflar/644cea6d4f3781369cb55fcd";
-const char* server8B = "http://192.168.1.102:5000/api/siniflar/644cea7a4f3781369cb55fce";
+const char* server8A = "http://172.16.101.22:5000/api/siniflar/644cea6d4f3781369cb55fcd";
+const char* server8B = "http://172.16.101.22:5000/api/siniflar/644cea7a4f3781369cb55fce";
+const char* serverKoridor = "http://172.16.101.22:5000/api/siniflar/645ccd504c4e0531b8b3804f";
 
 // LED pin numarası
-const int ledPin = D0;
-const int ledPin2 = D1;
+const int ledPin = D1;
+const int ledPin2 = D8;
+const int ledPin3 = D5;
 
 void setup() {
   // Seri port başlatma
@@ -29,37 +31,55 @@ void setup() {
   // LED pinini çıkış yapma
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 }
 
 void loop() {
   // HTTP isteği oluşturma
   HTTPClient http;
   HTTPClient http2;
+  HTTPClient http3;
   WiFiClient client;
   http.begin(client, server8A);
   http2.begin(client, server8B);
+  http3.begin(client, serverKoridor);
 
   // HTTP isteği gönderme ve yanıtı alıp yazdırma
   int httpCode = http.GET();
   int httpCode2 = http2.GET();
-  if (httpCode > 0 && httpCode2 > 0) {
+  int httpCode3 = http3.GET();
+
+  if (httpCode > 0 && httpCode2 > 0 && httpCode3 > 0) {
     String durum8A = http.getString();
     String durum8B = http2.getString();
+    String durumKoridor = http3.getString();
     Serial.print("8-A : ");
     Serial.println(durum8A);
     Serial.print("8-B : ");
     Serial.println(durum8B);
+    Serial.print("Koridor : ");
+    Serial.println(durumKoridor);
 
     // "led_durumu" adlı değişkenin değerine göre LED'i aç veya kapat
-    // if (durum8A == "true") {
-    //   digitalWrite(ledPin, HIGH);
-    //   digitalWrite(ledPin2, LOW);
-    //   Serial.println( durum8A);
-    // } else {
-    //   digitalWrite(ledPin, LOW);
-    //   digitalWrite(ledPin2, HIGH);
-    //   Serial.println( durum8B);
-    // }
+    if (durum8A == "true") {
+      digitalWrite(ledPin, HIGH);
+    }
+    else {
+      digitalWrite(ledPin, LOW);
+    }
+    if (durum8B == "true") {
+      digitalWrite(ledPin2, HIGH);      
+    }
+    else {
+      digitalWrite(ledPin2, LOW);
+    }
+    if (durumKoridor == "true"){
+      digitalWrite(ledPin3, HIGH);
+    }
+    else {
+      digitalWrite(ledPin3, LOW);
+    }
+
   } else {
     Serial.println("HTTP isteği başarısız.");
     Serial.println(httpCode);
@@ -67,5 +87,5 @@ void loop() {
 
   // HTTP isteğini sonlandırma ve 5 saniye bekleyerek tekrarlama
   http.end();
-  delay(10000);
+  delay(5000);
 }
